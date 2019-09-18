69e6adcec6075d467ac27aef9f98bc24afacf219
Kristina Davis <kristinadavis@kristinavisspro.dhcp.nd.edu>
2019-09-18 16:03:55 -0400

    Restyled homepage navbar & jumbotron

--- src/components/frontend/Navbar.vue
+++ src/components/frontend/Navbar.vue
@@ -1,55 +1,51 @@
 <template>
-    <div class="mt-1 mb-5">
-        <b-navbar type="dark" variant="dark">
-            <b-navbar-nav>
-                <b-nav-item :to="{name: 'home'}">CRC BVCC</b-nav-item>
-            </b-navbar-nav>
+  <div class>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <div class="container">
+        <b-navbar-brand href="#">CRC BVCC</b-navbar-brand>
 
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item :to="{name: 'dashboard'}">
-                    <i class="fas fa-th-large"></i>
-                    Dashboard
-                </b-nav-item>
-                <b-nav-item>|</b-nav-item>
-                <b-nav-item :to="{name: 'user'}">
-                    <i class="fas fa-user"></i>
-                    Profile
-                </b-nav-item>
-                <b-nav-item v-if="!user.email">|</b-nav-item>
-                <b-nav-item :to="{name: 'login', query: { next: '/dashboard'}}" v-if="!user.email">
-                    <i class="fas fa-key"></i>
-                    Sign In
-                </b-nav-item>
-                <b-nav-item :to="{name: 'register'}" v-if="!user.email">
-                    <i class="fas fa-user-plus"></i>
-                    Sign Up
-                </b-nav-item>
-            </b-navbar-nav>
-        </b-navbar>
-    </div>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :to="{name: 'dashboard'}">Dashboard</b-nav-item>
+            <b-nav-item :to="{name: 'dashboard'}">About</b-nav-item>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right no-caret="true"><template slot="button-content"><i class="fas fa-user-circle"></i></template>
+              <b-dropdown-item
+                :to="{name: 'login', query: { next: '/dashboard'}}"
+                v-if="!user.email"
+                >Sign In</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'register'}" v-if="!user.email">
+                  Sign Up
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-    import {
-        mapState
-    } from 'vuex';
+import { mapState } from "vuex";
 
-    export default {
-        data() {
-            return {}
-        },
-        computed: {
-            ...mapState('user', ['user'])
-        },
-        methods: {
-            toggle: function () {
-                this.drawer = !this.drawer;
-            }
-        }
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("user", ["user"])
+  },
+  methods: {
+    toggle: function() {
+      this.drawer = !this.drawer;
     }
+  }
+};
 </script>
 
 <style>
-
 </style>
