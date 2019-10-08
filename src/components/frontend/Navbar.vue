3291ea9846c207006c2e35187b1b68194174bc18
Kristina Davis <kfurse@nd.edu>
2019-10-08 11:47:30 -0400

    2nd round on dashboard

--- src/components/frontend/Navbar.vue
+++ src/components/frontend/Navbar.vue
@@ -8,7 +8,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item>About</b-nav-item>
-          <b-nav-item :to="{name: 'dashboard'}">Dashboard</b-nav-item>
+          <b-nav-item :to="{name: 'dashboard'}">Data Portal</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
