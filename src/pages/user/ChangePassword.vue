d52f4549167161c6ba623460cb30ebe49530b24c
Joseph McCombs <jmccombs@nd.edu>
2019-10-23 15:24:53 -0400

    Change password with errors displaying

--- src/pages/user/ChangePassword.vue
+++ src/pages/user/ChangePassword.vue
@@ -4,20 +4,20 @@
 
         <b-row class="my-3">
             <b-col sm="12" md="6" offset-md="3">
-                <b-form-input type="password" size="lg" v-model="form.password" placeholder="Password"></b-form-input>
+                <b-form-input type="password" size="lg" v-model="form.old_password" placeholder="Password"></b-form-input>
             </b-col>
         </b-row>
 
         <b-row class="my-3">
             <b-col sm="12" md="6" offset-md="3">
-                <b-form-input type="password" size="lg" v-model="form.new_password" placeholder="New Password">
+                <b-form-input type="password" size="lg" v-model="form.new_password1" placeholder="New Password">
                 </b-form-input>
             </b-col>
         </b-row>
 
         <b-row class="my-3">
             <b-col sm="12" md="6" offset-md="3">
-                <b-form-input type="password" size="lg" v-model="form.confirm_new_password"
+                <b-form-input type="password" size="lg" v-model="form.new_password2"
                     placeholder="Confirm Password"></b-form-input>
             </b-col>
         </b-row>
@@ -45,16 +45,16 @@
         mixins: [DashboardBaseTemplate],
         data: () => ({
             form: {
-                password: '',
-                new_password: '',
-                confirm_new_password: ''
+                old_password: '',
+                new_password1: '',
+                new_password2: ''
             }
         }),
         methods: {
             changePassword: function () {
                 let that = this;
 
-                if (!that.$data.form.password.trim()) {
+                if (!that.$data.form.old_password.trim()) {
                     that.$notify({
                         text: 'Please enter your current password.',
                         duration: 10000,
@@ -64,7 +64,12 @@
                     return;
                 }
 
+<<<<<<< 3288444e25346252c1fc9ab56974c3d000995830
                 if (!that.$data.form.new_password.trim() && (that.$data.form.new_password.trim() !== that.$data.form.confirm_new_password.trim())) {
+=======
+                if (!that.$data.form.new_password1.trim() && (that.$data.form.new_password1.trim() !== that.$data.form
+                        .new_password2.trim())) {
+>>>>>>> Change password with errors displaying
                     that.$notify({
                         text: 'New passwords do not match.',
                         duration: 10000,
@@ -84,11 +89,14 @@
                     that.$router.push({
                         name: 'user'
                     });
-                }).catch(() => {
-                    that.$notify({
-                        text: 'Could not change password.',
+                }).catch((error) => {
+                    let message = ('new_password2' in error.response.data) ? error.response.data.new_password2 : "could not reset your password";
+                    message.forEach(function(item) {
+                        that.$notify({
+                        text: item,
                         duration: 10000,
                         type: 'error'
+                        });
                     });
                 });
             },
