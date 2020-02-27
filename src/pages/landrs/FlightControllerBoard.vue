2656962a7863854e5bc425dbbf58509d0042db58
Sebastian Wyngaard <swyngaar@nd.edu>
2020-02-27 18:21:08 +0000

    Validate form data against SHACL criteria after submission

--- src/pages/landrs/FlightControllerBoard.vue
+++ src/pages/landrs/FlightControllerBoard.vue
@@ -3,21 +3,28 @@
         <b-container class="my-5">
             <h4 v-if="getFCB['@type']">{{ getFCB['@type'] | parseTitle }}</h4>
             <b-card bg-variant="light">
-                <b-form @submit="createNewInstance" v-if="showForm">
+                <b-form @submit.prevent="createNewInstance" v-if="showForm">
                     <b-form-group
                             v-for="item in fcbFormStructure" :key="item"
                             :label="`${item}:`"
                             :label-for="`input-${item}`"
                             label-cols-sm="1"
                     >
-                        <b-form-input
+                        <b-form-group
                                 v-if="getFCB[item].constructor.name === 'Object'"
-                                :id="`input-${item}`"
-                                type="text"
-                                required
-                                placeholder="Enter text"
-                                v-model="fcbNewInstanceData[item]"
-                        ></b-form-input>
+                                :invalid-feedback="fcbNewInstanceErrors[item]"
+                        >
+                            <b-form-input
+                                    :id="`input-${item}`"
+                                    type="text"
+                                    required
+                                    placeholder="Enter text"
+                                    v-model="fcbNewInstanceData[item]"
+                                    @input="fcbNewInstanceErrors[item] = null"
+                                    :state="!fcbNewInstanceErrors[item] ? fcbNewInstanceErrors[item] : false"
+                            ></b-form-input>
+                        </b-form-group>
+
                         <b-input-group
                                 v-if="getFCB[item].constructor.name === 'String'"
                                 prepend="http"
@@ -132,6 +139,7 @@
 <script>
 // @ is an alias to /src
 import BaseTemplate from '@/layouts/BaseTemplate.vue';
+import SHACLValidator from 'shacl'
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 
 const ignoredKeys = [
@@ -148,11 +156,12 @@ export default {
     return {
       fcbInstances: [],
       fcbNewInstanceData: {},
+      fcbNewInstanceErrors: {},
       showForm: true
     };
   },
   computed: {
-    ...mapGetters('landrs/fcb', ['getFCB']),
+    ...mapGetters('landrs/fcb', ['getFCB', 'getShape']),
     fcbFormStructure () {
       const structure = Object.keys(this.getFCB);
       return structure.filter(item => !ignoredKeys.includes(item));
@@ -160,8 +169,10 @@ export default {
   },
   mounted: async function () {
     this.setFCB(await this.fetchFCB());
+    this.setShape(await this.fetchShape());
     this.fcbInstances.push(this.getFCB);
     this.initFormData();
+    await this.validateInstanceData();
   },
   filters: {
     parseTitle (itemType) {
@@ -170,8 +181,8 @@ export default {
     }
   },
   methods: {
-    ...mapActions('landrs/fcb', ['fetchFCB']),
-    ...mapMutations('landrs/fcb', ['setFCB']),
+    ...mapActions('landrs/fcb', ['fetchFCB', 'fetchShape']),
+    ...mapMutations('landrs/fcb', ['setFCB', 'setShape']),
     instanceStructure (instance) {
       const structure = Object.keys(instance);
       return structure.filter(item => !ignoredKeys.includes(item));
@@ -186,10 +197,15 @@ export default {
         } else {
           this.fcbNewInstanceData = Object.assign({}, this.fcbNewInstanceData, { [entry]: '' });
         }
+        this.fcbNewInstanceErrors = Object.assign({}, this.fcbNewInstanceErrors, { [entry]: null });
       }
     },
-    createNewInstance(evt) {
-      evt.preventDefault();
+    async createNewInstance () {
+      // TODO Validate data before saving a new instance
+      if (!(await this.validateInstanceData())) {
+        return;
+      }
+
       // Transform object arrays into string arrays
       let newInstance = {};
       for (const entry of this.fcbFormStructure) {
@@ -217,6 +233,91 @@ export default {
       this.$nextTick(() => {
         this.showForm = true;
       })
+    },
+    async validateInstanceData () {
+      const mockInstanceData = `
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix geosparql: <http://www.opengis.net/ont/geosparql#> .
+@prefix wgs: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+@prefix sf: <http://www.opengis.net/ont/sf#> .
+@prefix schema: <http://schema.org/> .
+@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+@prefix sosa: <http://www.w3.org/ns/sosa/> .
+@prefix sosa-ext: <http://www.w3.org/ns/ssn/ext/> .
+@prefix ssn: <http://www.w3.org/ns/ssn/> .
+@prefix ssn-system: <http://www.w3.org/ns/ssn/systems/> .
+@prefix qudt-1-1: <http://qudt.org/1.1/schema/qudt#> .
+@prefix qudt-unit-1-1: <http://qudt.org/1.1/vocab/unit#> .
+@prefix envo: <http://purl.obolibrary.org/obo/> .
+@prefix ir: <http://www.ontologydesignpatterns.org/cp/owl/informationrealization.owl#> .
+@prefix st: <http://www.opengis.net/spec/iot_sensing/1.0/req/> .
+@prefix hydra: <http://www.w3.org/ns/hydra/core#> .
+#@prefix drone: <http://schema.landrs.org/schema/> .
+@prefix drone: <http://dirtforecast.com:33000/> .
+@prefix geoschemas: <http://geoschemas.org/> .
+@prefix td: <https://www.w3.org/2019/wot/td#> .
+@prefix eqp: <https://data.nasa.gov/ontologies/atmonto/equipment#> .
+@prefix atm: <https://data.nasa.gov/ontologies/atmonto/ATM#> .
+@prefix aero: <semantics.aero/pages/swim-vocabulary#> .
+@prefix traj: <http://w3id.org/daselab/onto/trajectory#> .
+@prefix step: <http://www.purl.org/net/step#> .
+@prefix wd: <http://www.wikidata.org/entity/> .
+@prefix wdt: <http://www.wikidata.org/prop/direct/> .
+
+<id/Mjc2MzRlZWUtZGRiYS00ZjE5LThjMDMtZDBmNDFjNmQzMTY0Cg==> a drone:FlightControllerBoard ;
+drone:isPartOf <id/YWUyMWRjMzAtOTA3NC00ZTYwLWI5ZTUtNjFhZmU1OTAzMTIyCg==> ;
+rdfs:label "CUAV 5+ Autopilot Board"@en ;
+rdfs:comment "V5+® is an advanced autopilot manufactured by CUAV®. It was designed by CUAV® in collaboration with the PX4 team."@en ;
+schema:sameAs <http://docs.px4.io/master/en/flight_controller/cuav_v5_plus.html> ;
+schema:Model "CUAV 5+"@en ;
+schema:description "fcb description" ;
+schema:identifier "fcb identifier" ;
+schema:name "fcb name" ;
+wdt:P31 wd:Q220858 ;
+sosa:hosts <id/OGIxYjVjOGEtOTgwZS00NDZhLTgzNTAtMzYyMzZlMzhjZDQ3Cg==> .
+
+<id/OGIxYjVjOGEtOTgwZS00NDZhLTgzNTAtMzYyMzZlMzhjZDQ3Cg==> a sosa:Sensor ;
+rdfs:label "TDK ICM-20629 Accelerometer-Gyroscope"@en ;
+rdfs:comment "ICM-20689 is a 6-axis MotionTracking device that combines a 3-axis gyroscope, 3-axis accelerometer" ;
+schema:sameAs <https://www.invensense.com/products/motion-tracking/6-axis/icm-20689/> ;
+schema:name "This is a name" ;
+schema:identifier "This is an identifier" ;
+schema:description "This is a description" ;
+wdt:P31 wd:Q192940 ;
+wdt:P31 wd:Q483400 ;
+sosa:isHostedBy <id/Mjc2MzRlZWUtZGRiYS00ZjE5LThjMDMtZDBmNDFjNmQzMTY0Cg==> ;
+sosa:observableProperty <http://sweetontology.net/propSpaceDirection/Orientation> ;
+sosa:observableProperty <http://sweetontology.net/propSpeed/Acceleration> .
+      `;
+
+      const report = await new Promise((resolve, reject) => {
+        const validator = new SHACLValidator();
+        validator.validate(mockInstanceData, 'text/turtle', this.getShape, 'text/turtle', function(e, report) {
+          if (e) {
+            reject(e);
+          }
+          resolve(report);
+        });
+      });
+
+      let noViolations = true;
+      if (!report.conforms()) {
+        const violations = report.results().filter(result => result.severity() === 'Violation');
+        violations.forEach((result) => {
+          console.log(`path: ${result.path()}`);
+          console.log(`severity: ${result.severity()}`);
+          console.log(`message: ${result.message()}`);
+          // TODO properly link the violation to the input element generated from the SHACL generated form
+          if (result.path().endsWith('name')) {
+            this.fcbNewInstanceErrors['label'] = result.message();
+            noViolations = false;
+          }
+        });
+      }
+
+      return noViolations;
     }
   }
 }
--- src/store/modules/landrs/fcb.js
+++ src/store/modules/landrs/fcb.js
@@ -5,21 +5,148 @@ import {
 export default {
   namespaced: true,
   state: {
-    fcb: {}
+    fcb: {},
+    shape: ''
   },
   getters: {
     getFCB (state) {
-      return state.fcb
+      return state.fcb;
+    },
+    getShape(state) {
+      return state.shape;
     }
   },
   mutations: {
     setFCB (state, payload) {
       state.fcb = payload;
+    },
+    setShape(state, payload) {
+      state.shape = payload;
     }
   },
   actions: {
     async fetchFCB () {
       const response = await HTTP.get('/schema/Thing/drone/fls/FlightControllerBoard?format=jsonld');
+      return response.data;
+    },
+    async fetchShape() {
+      // TODO replace mock response with async call to API to fetch Shape data
+      const response = {
+        data: `
+#@prefix landrs: <https://schema.landrs.org/schema/> .
+@prefix landrs: <http://dirtforecast.com:33000/> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix schema: <http://schema.org/> .
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix sosa: <http://www.w3.org/ns/sosa/> .
+@prefix sosa-ext: <http://www.w3.org/ns/ssn/ext/> .
+@prefix ssn: <http://www.w3.org/ns/ssn/> .
+@prefix ssn-system: <http://www.w3.org/ns/ssn/systems/> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+
+landrs:FlightControllerBoardShape
+  a sh:NodeShape ;
+  sh:targetClass landrs:FlightControllerBoard ;
+####
+# FlightControllerBoard mandatory properties
+####
+sh:property [
+      sh:path schema:description ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+      sh:datatype xsd:string ;
+ ] ;
+ sh:property [
+      sh:path schema:identifier ;
+      sh:or ( [ sh:datatype xsd:string ; ]
+        [ sh:datatype xsd:anyURI ; ] ) ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+ ] ;
+ sh:property [
+      sh:path schema:name ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+      sh:datatype xsd:string ;
+ ] ;
+ sh:property [
+   sh:path sosa:hosts ;
+   sh:NodeKind sh:IRI ;
+   sh:class sosa:Sensor ;
+   sosa:minCount 1 ;
+ ] ;
+####
+# FlightControllerBoard recommended properties schema.org
+####
+ sh:property [
+      sh:path schema:manufacturer ;
+      sh:class schema:Organization ;
+ ] ;
+ sh:property [
+      sh:path schema:manufacturer ;
+      sh:class schema:Organization ;
+      sh:minCount 1 ;
+      sh:message "Manufacturer is recommended. Please fill in a value"@en ;
+      sh:severity sh:Warning ;
+ ] ;
+ sh:property [
+      sh:path schema:serialNumber ;
+      sh:datatype xsd:string ;
+      sh:maxCount 1 ;
+ ] ;
+ sh:property [
+      sh:path schema:serialNumber ;
+      sh:datatype xsd:string ;
+      sh:minCount 1 ;
+      sh:message "SerialNumber is recommended. Please fill in a value"@en ;
+      sh:severity sh:Warning ;
+ ] ;
+####
+# Equipment optional properties
+####
+ #sh:property [
+ #     sh:path dct:isPartOf ;
+ #     sh:or ( [ sh:class epos:Equipment ; ]
+ #       [ sh:class epos:Facility ; ] );
+ #] ;
+.
+
+
+landrs:sensorShape
+  a sh:NodeShape ;
+  sh:targetClass sosa:Sensor ;
+sh:property [
+  sh:path sosa:observes ;
+  sh:NodeKind sh:IRI ;
+  sh:class sosa:ObservableProperty ;
+  sosa:minCount 1 ;
+] ;
+sh:property [
+      sh:path schema:description ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+      sh:datatype xsd:string ;
+ ] ;
+ sh:property [
+      sh:path schema:identifier ;
+      sh:or ( [ sh:datatype xsd:string ; ]
+        [ sh:datatype xsd:anyURI ; ] ) ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+ ] ;
+ sh:property [
+      sh:path schema:name ;
+      sh:minCount 1 ;
+      sh:maxCount 1 ;
+      sh:datatype xsd:string ;
+ ] ;
+.
+        `
+      };
+
       return response.data;
     }
   },
